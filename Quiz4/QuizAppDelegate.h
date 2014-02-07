04e5ff29b50da9d20f37454a9daa6c4aa4ee28a2
Brandon <brantauber@Brandons-MacBook-Pro-2.local>
2014-02-06 19:30:50 -0500

    Initial Commit

---
+++ Quiz4/QuizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizAppDelegate.h
+//  Quiz4
+//
+//  Created by Brandon on 2/6/14.
+//  Copyright (c) 2014 Brandon Tauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz4
+//
+//  Created by Brandon on 2/6/14.
+//  Copyright (c) 2014 Brandon Tauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QuizAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QuizAppDelegate class]));
+    }
+}
